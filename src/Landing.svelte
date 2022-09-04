f6db7e20beaccc5150eb206ae69bb5a8609ac5a5
Amat0l <tianlynn.wang@gmail.com>
2022-09-04 16:24:42 -0400

    first commit

---
+++ src/Landing.svelte
@@ -0,0 +1,17 @@
+<script>
+	/*js*/
+	
+</script>
+<body>
+	
+	<main>
+		<h1>BLT</h1>
+		<p>blt website </p>
+	</main>
+</body>
+
+<style>
+
+	/* style*/
+	
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Landing from './Landing.svelte';
+
+
+const landing = new Landing({
+	target: document.body
+	
+});
+
+export default app;
